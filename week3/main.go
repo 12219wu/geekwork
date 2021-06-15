08f5f3a15e80fe2dcf91046b9e912ef02813aa6e
wuyadong <1002841782@qq.com>
2021-06-15 11:40:50 +0800

    第三周作业

--- week3/main.go
+++ week3/main.go
@@ -14,6 +14,7 @@ import (
 )
 
 func main() {
+	log.SetFlags(log.Lshortfile)
 	//ctx, cancel := xContext.WithCancel(xContext.Background())
 	ctx, cancel := context.WithCancel(context.Background())
 	group, errCtx := errgroup.WithContext(ctx)
